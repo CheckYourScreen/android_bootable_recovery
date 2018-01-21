21ad3a3c92b1a70433ff830bbb54d33ec6a04768
CheckYourScreen <nimitmehta95@gmail.com>
2018-01-21 17:23:52 +0100

    TWRP-3.2.1-r18
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>

--- variables.h
+++ variables.h
@@ -17,7 +17,7 @@
 #ifndef _VARIABLES_HEADER_
 #define _VARIABLES_HEADER_
 
-#define TW_VERSION_STR              "3.2.1-r17"
+#define TW_VERSION_STR              "3.2.1-r18"
 
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
